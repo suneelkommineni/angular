29a15f634b273d60432b807d2eb1c5b79d8687f3
KSPL-HYD <saran.saladi@kelltontech.com>
2019-02-14 17:25:15 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AlertModule } from 'ngx-bootstrap';
 import { RouterModule, Routes } from '@angular/router';
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SocialmediaiconsComponent } from './socialmediaicons/socialmediaicons.component';
-
-
-
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BlogComponent } from './blog/blog.component';
+import { FeaturesComponent } from './features/features.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
+    {path: '', pathMatch: 'full',  redirectTo: 'home'},
+    {path: 'login', component: LoginComponent,},
+    {path: 'home',component: HomepageComponent},
+    {path: 'aboutus',component: AboutusComponent},
+    {path: 'portfolio',component: PortfolioComponent},
+    {path: 'blog',component: BlogComponent},
+    {path: 'featuers',component: FeaturesComponent},
+    {path: 'contact',component: ContactComponent},
   
-  {
-      path: '',
-      component: AppComponent,
-  },
-
-  {
-    path: 'login',
-    component: LoginComponent,
-}];
+  ];
 
 
 
@@ -36,7 +36,13 @@ const routes: Routes = [
     LoginComponent,
     HeaderComponent,
     FooterComponent,
-    SocialmediaiconsComponent
+    SocialmediaiconsComponent,
+    HomepageComponent,
+    AboutusComponent,
+    PortfolioComponent,
+    BlogComponent,
+    FeaturesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,12 +4,40 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
+  
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  title: string;
+  myHero: string;
+  dataset:any;
+
+  constructor() {
+
+    this.title = 'Tour of Heroes';
+    this.myHero = 'Windstorm';
+
+    this.dataset = [
+      {
+      "name":"Suneel",
+      "age":30,
+      "cars":[ "Ford", "BMW", "Fiat" ],
+      "prevEXp":[{"company":"synvers","years":"2"},{"company":"ipay","years":"2"}]
+      },
+      {
+      "name":"sai",
+      "age":20,
+      "cars":[ "indica", "Baleno", "shift" ],
+      "prevEXp":[{"company":"IBM","years":"1"},{"company":"kellton","years":"0"}]
+      }
+      ];  
+      
+   }
 
   ngOnInit() {
+
+  
+
   }
 
 }
